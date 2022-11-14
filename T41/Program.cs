78c9c1c3ca939fde4197d48543cd18de47ce3c0c
Elena <zagre8elena@yandex.ru>
2022-11-14 15:48:25 +0300

    added T41

---
+++ T41/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}");
+}
+int SumPositiveNumber(int[] array){
+    int count = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]>0) count++;
+    }
+    return count;
+}
+Console.Write("Введите количество элементов в массиве:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Введите c клавиатуры {n} чисел  ");
+int[] somearray = new int[n];
+for (int i = 0; i < somearray.Length; i++)
+{
+    somearray[i] = Convert.ToInt32(Console.ReadLine());
+}
+PrintArray(somearray);  
+Console.Write($" -> {SumPositiveNumber(somearray)}");
